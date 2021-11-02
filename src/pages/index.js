d50ed48b7140941b29d737cfb21da3560ddd35b9
Beverley Sun <beverleysun0205@gmail.com>
2021-11-02 19:36:21 +1300

    Initial commit

---
+++ src/pages/index.js
@@ -0,0 +1,12 @@
+import * as React from "react";
+import "../styles/styles.scss";
+
+const Index = () => {
+  return (
+    <div>
+      <h1>Hello</h1>
+    </div>
+  );
+};
+
+export default Index;
