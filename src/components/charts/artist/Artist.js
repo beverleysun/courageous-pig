37d99394983152b8486ebbd0eaf234e5017088d8
Beverley Sun <beverleysun0205@gmail.com>
2021-11-05 03:03:06 +1300

    Fix row expansion

--- src/components/charts/artist/Artist.js
+++ src/components/charts/artist/Artist.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { Img } from "react-image";
+import Summary from "./Summary";
 import Increase from "../../../assets/artist-ranking/increase.svg";
 import Decrease from "../../../assets/artist-ranking/decrease.svg";
 import NoChange from "../../../assets/artist-ranking/no-change.svg";
@@ -12,27 +13,34 @@ const Artist = (props) => {
 
   return (
     <div className={`artist-row ${expanded ? "expanded" : ""}`}>
-      <Img
-        className="photo"
-        src="https://i.scdn.co/image/ab6761610000f17812a2ef08d00dd7451a6dbed6"
-        loading="lazy"
-        alt="artist"
-      />
+      <div className="row">
+        <Img
+          className="photo"
+          src="https://i.scdn.co/image/ab6761610000f17812a2ef08d00dd7451a6dbed6"
+          loading="lazy"
+          alt="artist"
+        />
 
-      <div className="row-info" onClick={() => setExpanded(!expanded)}>
-        <div className="collapsed-ranking">
-          <p className="ranking">2</p>
-          <Increase className="change-icon" />
+        <div className="row-info" onClick={() => setExpanded(!expanded)}>
+          <div className="collapsed-ranking">
+            <p className="ranking">2</p>
+            <Increase className="change-icon" />
+          </div>
+
+          <h2 className="artist-name">Ed Sheeran</h2>
+
+          <p className="monthly-listeners">74,383,532 Monthly Listeners</p>
+          <div className="divider"></div>
+          <ArrowExpand className="arrow-expand" />
         </div>
-
-        <h2 className="artist-name">Ed Sheeran</h2>
-
-        <p className="monthly-listeners">74,383,532 Monthly Listeners</p>
-        <div className="divider"></div>
-        <ArrowExpand className="arrow-expand" />
       </div>
 
-      <div className="expanded-ranking"></div>
+      <div className="expanded-content">
+        <div className="left">
+          <div className="ranking">asdddddddddddd</div>
+          <Summary></Summary>
+        </div>
+      </div>
     </div>
   );
 };
