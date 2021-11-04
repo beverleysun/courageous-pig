b71d6fd35194961f61676c1f716e899a3e88cf1d
Beverley Sun <beverleysun0205@gmail.com>
2021-11-05 02:23:51 +1300

    Stick mobile pagination to bottom

--- src/components/charts/Charts.js
+++ src/components/charts/Charts.js
@@ -1,26 +1,32 @@
 import React from "react";
 import Pagination from "./Pagination";
 import RootLayout from "../RootLayout";
+import Artist from "./artist/Artist";
 import "../../styles/charts/charts.scss";
 
 const Charts = ({ title, date }) => {
   return (
-    <RootLayout title={title} subtitle={`You are viewing ${date}`}>
-      <div className="controls">
-        <div className="search">
-          <label htmlFor="search">Search Artist : </label>
-          <input name="search" />
+    <>
+      <RootLayout title={title} subtitle={`You are viewing ${date}`}>
+        <div className="controls">
+          <div className="search">
+            <label htmlFor="search">Search Artist : </label>
+            <input name="search" />
+          </div>
+          <Pagination
+            links={{
+              previousDay: "2021-11-04",
+              previousWeek: "2021-11-04",
+              nextDay: "2021-11-04",
+              nextWeek: "2021-11-04",
+            }}
+          ></Pagination>
         </div>
-        <Pagination
-          links={{
-            previousDay: "2021-11-04",
-            previousWeek: "2021-11-04",
-            nextDay: "2021-11-04",
-            nextWeek: "2021-11-04",
-          }}
-        ></Pagination>
-      </div>
-      <div className="list-items"></div>
+        <div className="artists">
+          <Artist />
+          <Artist />
+        </div>
+      </RootLayout>
       <Pagination
         className="mobile"
         links={{
@@ -30,7 +36,7 @@ const Charts = ({ title, date }) => {
           nextWeek: "2021-11-04",
         }}
       ></Pagination>
-    </RootLayout>
+    </>
   );
 };
 
